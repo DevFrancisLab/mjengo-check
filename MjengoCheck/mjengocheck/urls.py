c7e06e2c53e6eb6f1693382bd03642ef6b08588c
Francis <francmaki14@gmail.com>
2025-04-15 11:35:41 +0300

    create mjengocheck app

---
+++ MjengoCheck/mjengocheck/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='mjengocheck-home'),
+    path('about/',views.about, name='mjengocheck-about'),
+]
