ba5c9484dc53d1717e24230032314e4e3001ac99
Francis <francmaki14@gmail.com>
2025-04-17 00:53:04 +0300

    create models

--- MjengoCheck/mjengocheck/admin.py
+++ MjengoCheck/mjengocheck/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Site, Worker, CheckInLog, IncidentReport
+
+admin.site.register(Site)
+admin.site.register(Worker)
+admin.site.register(CheckInLog)
+admin.site.register(IncidentReport)
--- MjengoCheck/mjengocheck/models.py
+++ MjengoCheck/mjengocheck/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+class Worker(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+
+class CheckInLog(models.Model):
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[('in', 'Check In'),('out','Check Out')])
+
+class IncidentReport(models.Model):
+    worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateField(auto_now_add=True)
